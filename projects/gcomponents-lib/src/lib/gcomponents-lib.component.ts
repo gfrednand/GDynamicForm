40e70d6f98d80fd09c539ca2f02d83b1a29f39d5
Chami <gilbert.chami@ega.go.tz>
2021-02-15 18:38:19 +0300

    changing prefix

--- projects/gcomponents-lib/src/lib/gcomponents-lib.component.ts
+++ projects/gcomponents-lib/src/lib/gcomponents-lib.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'cmpts-GComponentsLib',
+  selector: 'app-GComponentsLib',
   template: `
     <p>
       gcomponents-lib works!
